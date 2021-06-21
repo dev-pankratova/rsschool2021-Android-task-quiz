d1ad6455df48e3e9f570d83ef4f46dfd1876d371
Olesya Pankratova <pan.develop89@gmail.com>
2021-06-22 01:06:30 +0600

    feat: init commit

--- app/src/main/java/com/rsschool/quiz/MainActivity.kt
+++ app/src/main/java/com/rsschool/quiz/MainActivity.kt
@@ -7,5 +7,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        attachFragment()
     }
+
+    private fun attachFragment() {
+        val fragment = FragmentQuiz.newInstance()
+        val transaction = supportFragmentManager.beginTransaction().apply {
+            replace(R.id.container, fragment)
+            commit()
+        }
+    }
+
+
 }
